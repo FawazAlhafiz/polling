49a37f709e7b32f629fca1f9c88850122a8d3f4e
Fawaz Alhafiz <fawaztwix111@gmail.com>
2025-07-21 21:14:26 +0300

    feat: create virtual doc to show results

--- polling/polling/doctype/poll_result/poll_result.py
+++ polling/polling/doctype/poll_result/poll_result.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, Fawaz Alhafiz and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -11,14 +11,35 @@ class PollResult(Document):
 		pass
 
 	def load_from_db(self):
-		pass
+		# This is where we compute results on demand
+		poll_name = self.name  # For virtual doctypes, name is passed in filter
+		poll = frappe.get_doc("Poll", poll_name)
+
+		total_votes = sum(option.vote_count for option in poll.options)
+
+		self.options = []
+		for option in poll.options:
+			percent = (option.vote_count / total_votes * 100) if total_votes > 0 else 0
+			self.options.append({
+				'option_text': option.option_text,
+				'vote_count': option.vote_count,
+				'percentage': round(percent, 1)
+			})
+
+		self.total_votes = total_votes
+		self.poll_title = poll.title
 
 	def db_update(self):
 		pass
 
 	@staticmethod
-	def get_list(args):
-		pass
+	def get_list(filters):
+		# Return minimal info if listing multiple results
+		return [{
+			'name': poll.name,
+			'poll_title': poll.title,
+			'total_votes': sum(option.vote_count for option in frappe.get_all("Poll Option", filters={'parent': poll.name}, fields=['vote_count']))
+		} for poll in frappe.get_all("Poll", fields=['name', 'title'])]
 
 	@staticmethod
 	def get_count(args):
