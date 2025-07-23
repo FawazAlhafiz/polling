d47c7a4346160f78462f36243059e016f9e6cbfd
Fawaz Alhafiz <fawaztwix111@gmail.com>
2025-07-23 22:47:39 +0300

    feat: apply form filters on link fields

--- polling/polling/doctype/poll_vote/poll_vote.js
+++ polling/polling/doctype/poll_vote/poll_vote.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2025, Fawaz Alhafiz and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Poll Vote", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Poll Vote", {
+	setup(frm) {
+        frm.set_query("option", function() {
+            return {
+                filters: {
+                    parent: frm.doc.poll,
+                }
+            };
+        });
+    },
+    refresh(frm) {
+        
+	},
+});
