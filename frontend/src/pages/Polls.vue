f07addf6df33d27fe9883247bda00ecb4a1495ce
Fawaz Alhafiz <fawaztwix111@gmail.com>
2025-07-29 01:30:18 +0300

    chore: setting up polls list resource

---
+++ frontend/src/pages/Polls.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold">👌polls page</h1>
+        
+        <div v-if="pollsResource.list.data">
+            <p v-for="poll in pollsResource.list.data" :key="poll.name">{{ poll.title }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { createListResource } from 'frappe-ui';
+
+const pollsResource = createListResource({
+    doctype: 'Poll',
+    fields: ['name', 'title', 'description', 'start_date', 'end_date'],
+    auto: true,
+});
+</script>
