5dce32d137defcd78af42e8bd1286e1ec17a08a7
fawaaaz111 <fawaztwix111@gmail.com>
2025-08-26 12:26:33 +0300

    Configure gitignore for build assets and remove tracked build files
    - Add polling/public/frontend/assets/ to .gitignore
    - Remove previously tracked build assets from git index
    - Update HTML files with new asset references after build
    - Build assets will now be generated locally and not tracked in git

--- polling/public/frontend/assets/Home-Bq2mkYHT.js
+++
@@ -1,2 +0,0 @@
-import{c as e,r,a,o}from"./index-upOFdLO4.js";const n={__name:"Home",setup(t){return e({url:"ping",auto:!0}),r(!1),(c,s)=>(o(),a("h1",null,"Polling"))}};export{n as default};
-//# sourceMappingURL=Home-Bq2mkYHT.js.map
