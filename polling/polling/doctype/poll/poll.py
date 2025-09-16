1a9a2ba9dc3ebddefc695ee32706f1ac3aeb2516
fawaaaz111 <fawaaaz111@gmail.com>
2025-09-16 04:16:11 +0300

    chore: made validation on title field to not contain special character

--- polling/polling/doctype/poll/poll.py
+++ polling/polling/doctype/poll/poll.py
@@ -2,8 +2,12 @@
 # For license information, please see license.txt
 
 # import frappe
+import re
 from frappe.model.document import Document
 
-
 class Poll(Document):
-	pass
+	def validate(self):
+		# Allow only alphanumeric, spaces, hyphens, and underscores in title
+		if self.title and not re.match(r'^[\w\- ]+$', self.title):
+			from frappe import throw, _
+			throw(_("Title cannot contain special characters. Only letters, numbers, spaces, hyphens, and underscores are allowed."))
