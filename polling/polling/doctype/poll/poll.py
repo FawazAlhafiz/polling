83ad93d839600c21665ad54afda8c339c64faf38
FawazAlhafiz <fawaztwix111@gmail.com>
2026-03-19 15:19:25 +0300

    chore: better imports

--- polling/polling/doctype/poll/poll.py
+++ polling/polling/doctype/poll/poll.py
@@ -1,13 +1,12 @@
 # Copyright (c) 2025, Fawaz Alhafiz and contributors
 # For license information, please see license.txt
 
-# import frappe
 import re
+from frappe import throw, _
 from frappe.model.document import Document
 
 class Poll(Document):
 	def validate(self):
 		# Allow only alphanumeric, spaces, hyphens, and underscores in title
 		if self.title and not re.match(r'^[\w\- ]+$', self.title):
-			from frappe import throw, _
 			throw(_("Title cannot contain special characters. Only letters, numbers, spaces, hyphens, and underscores are allowed."))
