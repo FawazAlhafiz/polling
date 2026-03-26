069c4a757c0dca1d952694c7d8b238851fb866f5
FawazAlhafiz <fawaztwix111@gmail.com>
2026-03-27 01:48:56 +0300

    fix: implement end_date auto-correction to 23:59:59 for all-day polls

--- polling/polling/doctype/poll/poll.js
+++ polling/polling/doctype/poll/poll.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2025, Fawaz Alhafiz and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Poll", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on('Poll', {
+	end_date(frm) {
+		// When the user picks a date without setting a time, Frappe stores
+		// 00:00:00. Auto-correct to 23:59:59 so the poll is available all
+		// day on the expiry date.
+		if (frm.doc.end_date && frm.doc.end_date.endsWith('00:00:00')) {
+			frm.set_value('end_date', frm.doc.end_date.substring(0, 10) + ' 23:59:59');
+		}
+	},
+});
