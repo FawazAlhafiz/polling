d151047e960145381c1f570831486aa2e2360214
FawazAlhafiz <fawaztwix111@gmail.com>
2026-03-27 01:50:02 +0300

    feat: add notification fields for poll expiry management

--- polling/polling/doctype/poll/poll.py
+++ polling/polling/doctype/poll/poll.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import re
+import frappe
 from frappe import throw, _
 from frappe.model.document import Document
 
@@ -10,3 +11,11 @@ class Poll(Document):
 		# Allow only alphanumeric, spaces, hyphens, and underscores in title
 		if self.title and not re.match(r'^[\w\- ]+$', self.title):
 			throw(_("Title cannot contain special characters. Only letters, numbers, spaces, hyphens, and underscores are allowed."))
+		self._reset_notification_flag_if_end_date_changed()
+
+	def _reset_notification_flag_if_end_date_changed(self):
+		if self.is_new():
+			return
+		old_end_date = frappe.db.get_value("Poll", self.name, "end_date")
+		if old_end_date != self.end_date:
+			self.expiry_notification_sent = 0
